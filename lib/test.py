bc354f18d7618b58ed477b90cb6b317f2b54ae7c
voiteshonok <vsvoiteshonok1@gmail.com>
2021-04-04 11:25:42 +0300

    made reset

---
+++ lib/test.py
@@ -0,0 +1,15 @@
+import Serializer
+
+serializer = Serializer.Serializer()
+serializer.change_form('json')
+
+ar = [serializer, {"12": 45}]
+serializer.data = ar
+
+serializer.dumps()
+
+print(serializer.string)
+
+serializer.loads()
+
+print(serializer.data)
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lib',
+    packages=find_packages(include=['lib']),
+    version='0.1.0',
+    description='Serializer/Deserializer',
+    author='Me',
+    license='MIT',
+    python_requires='>=3.8',
+)
+'''
+import os
+from pathlib import Path
+
+home = str(Path.home())
+
+os.system('rm -rf ~/lib')
+os.system('mkdir ~/lib')
+os.system('cp -a . ~/lib')
+os.system('chmod +x ~/lib/main.py')
+with open(home + '/.bashrc', 'a') as file:
+    file.write("alias redump='" + "~/lib/main.py'")
+    '''
