b28a77a154b92b6e9e8da43ebf09f84c67c48017
denchidlo <twihkapb@gmail.com>
2021-04-04 13:29:08 +0300

    Parser sources

---
+++ pyparse_pkg/serialiser_test.py
@@ -0,0 +1,7 @@
+from unittests import objects as res
+from pyparse import *
+import unittest
+
+class SerrializerTester(unittest):
+    def __init__(self) -> None:
+        self.test_data = res.samples
