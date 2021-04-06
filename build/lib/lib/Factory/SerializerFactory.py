39ccfc1305b81ca86fa86aaf50e92cac56bf92f6
voiteshonok <vsvoiteshonok1@gmail.com>
2021-04-06 19:49:24 +0300

    cli done

---
+++ build/lib/lib/Factory/SerializerFactory.py
@@ -0,0 +1,26 @@
+from JsonParser import JsonParser
+from TomlParser import TomlParser
+from YamlParser import YamlParser
+from PickleParser import PickleParser
+
+
+class SerializerFactory:
+    def __init__(self):
+        self._creators = {}
+
+    def create_serializer(self, format, creator):
+        self._creators[format.lower()] = creator
+
+    def get_serializer(self, format):
+        creator = self._creators.get(format.lower())
+        if not creator:
+            raise ValueError(format)
+        return creator()
+
+
+factory = SerializerFactory()
+
+factory.create_serializer('JSON', JsonParser.JsonParser)
+factory.create_serializer('Toml', TomlParser.TomlParser)
+factory.create_serializer('Yaml', YamlParser.YamlParser)
+factory.create_serializer('Pickle', PickleParser.PickleParser)
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='lib',
-    packages=find_packages(include=['lib']),
-    version='0.1.0',
+    packages=find_packages(),
+    version='0.1.25',
     description='Serializer/Deserializer',
     author='Me',
     license='MIT',
     python_requires='>=3.8',
 )
-'''
+
 import os
 from pathlib import Path
 
@@ -18,7 +18,7 @@ home = str(Path.home())
 os.system('rm -rf ~/lib')
 os.system('mkdir ~/lib')
 os.system('cp -a . ~/lib')
-os.system('chmod +x ~/lib/main.py')
+os.system('chmod +x ~/lib/lib/redump.py')
 with open(home + '/.bashrc', 'a') as file:
-    file.write("alias redump='" + "~/lib/main.py'")
-    '''
+    file.write("alias redump='" + "~/lib/lib/redump.py'")
+
